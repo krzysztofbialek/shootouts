e731d180a8b582a042b4a38710a811b741d277ad
krzysztofbialek <zlo@o2.pl>
2012-12-12 14:03:44 +0100

    start game with default values

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,10 +1,15 @@
 class Game
 
   cattr_accessor :games
+  attr_accessor :winner, :num, :shots, :start_with
 
   @@games = {}
 
   def initialize
+    @num  = @@games.size
+    @start_with = rand(2) == 1? 'shot' : 'save'
+    @shots = {}
+    
     @@games.merge!(@@games.size => self)
   end
 
--- test/unit/game_test.rb
+++ test/unit/game_test.rb
@@ -14,4 +14,12 @@ class GameTest < Test::Unit::TestCase
   should "games hash should be empty at start of server" do
     assert_equal 0, Game.games.size, "Number of games was #{Game.games.size} instead of 0"
   end 
+
+  should "create game with default starting values" do
+    game = Game.new
+    assert_equal nil, game.winner
+    assert ['shot', 'save'].include?(game.start_with), game.start_with
+    assert_equal Game.games.size - 1, game.num
+  end
+
 end
