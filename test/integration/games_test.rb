0c5a4c5076d2c1e4f77872432a36a434f2b1343f
krzysztofbialek <zlo@o2.pl>
2012-12-12 14:40:49 +0100

    create game with post call and respond with json

--- test/integration/games_test.rb
+++ test/integration/games_test.rb
@@ -1,8 +1,18 @@
 require 'test_helper'
 
 class GamesTest < ActionController::IntegrationTest
+  
   should 'create new game' do
     post api_games_url, :format => :json
     assert_response :success
   end
+
+  should 'respond with valid game' do
+    post api_games_url, :format => :json
+    new_game = JSON.parse(response.body)
+    game = Game.games[new_game['num']]
+    assert_equal game.num, new_game['num']
+    assert_equal game.start_with, new_game['start_with']
+  end
+  
 end
